bb5f7b18d12452e9e2c60e946e48a79777310258
SabineMarwitz <158596766+SabineMarwitz@users.noreply.github.com>
2025-08-25 18:10:50 +0200

    add openai and forward user message

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,52 @@
-require('dotenv').config();
-
 import express from 'express';
 import cors from 'cors';
+import OpenAI from "openai";
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors()); 
+app.use(cors());
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
 
 app.get("/", (req, res) => {
-  res.send("Hello World!!!");
+  res.send("Hello World!");
+});
+
+app.post('/api/data', async (req, res) => {
+  // accept user message
+  const requestData = req.body;
+  console.log('Received JSON:', requestData);
+  console.log();
+
+  // call ChatGPT
+  const response = await client.responses.create({
+    model: 'gpt-5-nano',
+    instructions: 'You are a helpful tutor', 
+    input: requestData.question, 
+  });
+
+  console.log(response.output_text);
+  
+
+  // save user message and assistant response into a database
+
+  // return assistant response as JSON
+  res.json({ 
+    User: requestData.question, 
+    Assistant: response.output_text
+  });
+
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
   console.log(
-    `Open http://localhost:${PORT} in your browser to see SSE in action.`
+    `Open http://localhost:${PORT} in your browser.`
   );
 });
